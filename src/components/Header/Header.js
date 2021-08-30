b643dde3e4c92ec35076a21b15c9e7040a364041
Rijwanul ALam <rijwanul007@gmail.com>
2021-08-31 03:34:17 +0600

    added header design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
-
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 const Header = () => {
     return (
         <div className="header">
             <img className="headerLogo" src="https://logos-download.com/wp-content/uploads/2016/03/Amazon_Logo_2000.png" alt="amazon" />
             <div className="headerSearch">
-                <input className="headerSearchIn" type="text" />
+                <input className="headerSearchInput" type="text" />
+                <SearchIcon className="headerSearchIcon" />
             </div>
 
             <div className="headerNav">
@@ -23,6 +25,11 @@ const Header = () => {
                 <div className="headerOption">
                     <span className="headerOptionLineOne">Your</span>
                     <span className="headerOptionLineTwo">Prime</span>
+                </div>
+
+                <div className="headerOptionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="headerOptionLineTwo headerBasketCount">0</span>
                 </div>
             </div>
         </div>
